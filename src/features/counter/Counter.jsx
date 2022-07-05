b0dc4edb5f1595a6ebc610f5dfc1f7f81653ffdf
Kba05 <babahan-98@mail.ru>
2022-07-05 13:25:47 +0300

    lesson 2 from redux

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './counterSlice'
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  selectCount,
+} from './counterSlice';
 
 export function Counter() {
-  const count = useSelector(state => state.counter.value)
-  const dispatch = useDispatch()
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
 
   return (
-      <div>
-            <button onClick={() => dispatch(increment())}>
-            Increment
-            </button>
-            
-            <span>{count}</span>
-
-            <button onClick={() => dispatch(decrement())}>
-            Decrement
-            </button>
-      </div>
-  )
+    <div>
+        <div>
+          <button onClick={() => dispatch(increment())}>
+            +
+          </button>
+          <span>{count}</span>
+          <button onClick={() => dispatch(decrement())}>
+            -
+          </button>
+        </div>
+        <input value={incrementAmount} onChange={e => setIncrementAmount(e.target.value)}/>
+        <button onClick={() => dispatch(incrementByAmount(Number(incrementAmount) || 0))}>
+          Add Amount
+        </button>
+        <button onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}>
+          Add Async
+        </button>
+    </div>
+  );
 }
